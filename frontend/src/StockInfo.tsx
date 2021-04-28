2792c177dd7eb622286692cfa1225fbcf57d4944
Willen Zhou <wz283@cornell.edu>
2021-04-28 19:38:17 -0400

    stock info

--- frontend/src/StockInfo.tsx
+++ frontend/src/StockInfo.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import StockScreen from './StockScreen';
 
 export type StockInfo = {
     readonly name: string,
     readonly price: number,
     readonly favorite: boolean,
     readonly num_shares: number
-  }
-  
+}
+
 const StockInfo = () => {
-    return(
+    return (
         <div>
-            StockInfo
+            Stock Information
+            {/* <StockScreen stock={} /> need backend */}
+
         </div>
     )
 }
--- frontend/src/StockScreen.tsx
+++ frontend/src/StockScreen.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { StockInfo } from './StockInfo';
+import FavoriteStock from './FavoriteStock';
 
-const StockScreen = () => {
+const StockScreen = (stock: StockInfo) => {
+  const [currstock, setCurrstock] = useState<StockInfo>(stock);
+
+  const changeStock = (s: StockInfo) => {
+    setCurrstock(s);
+  }
+
+  const name = stock.favorite ? (
+    stock.name
+  ) : (
+    <span style={{ color: 'red' }}>{stock.name}</span>
+  );
 
   return (
     <div>
-
+      Name: {name}
+      Price: {stock.price}
+      <FavoriteStock stock={currstock} callback={changeStock} />
     </div>
   )
 }
